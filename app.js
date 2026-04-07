a534cfca190089d7dff57443356723e7618b6a73
ernestocostanzo51 <ernestanzo51@gmail.com>
2026-04-07 15:21:53 +0200

    creazione data con tutti i post

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const postsRouter = require('./routers/posts')
+const postsRouter = require('./routers/postsRouter')
 
 app.use('/posts', postsRouter)
 
